5b136b997b17d9961ae8b7aa11f56b0253d0d0f7
Konrad Kasperczyk <kkasperczyk20@gmail.com>
2020-07-21 16:08:47 +0200

    added Container component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,81 @@
-import React from 'react';
+import React from "react";
+import Container from "./Container";
 
 function App() {
   return (
-    <div>
-      Dzień dobry!
-    </div>
+    <Container>
+      <header className="header">
+        <h1 className="header__title">Currency Converter</h1>
+      </header>
+      <main className="main">
+        <form className="form js-form">
+          <p>
+            <label>
+              <span className="form__labelText">Amount:</span>
+              <input
+                className="form__field form__field--bigger form__amount js-form__amount"
+                type="number"
+                name="amount"
+                min="1"
+                step="any"
+                required
+                autoFocus
+                placeholder="Enter amount"
+              />
+            </label>
+          </p>
+          <p>
+            <label>
+              <span className="form__labelText">From:</span>
+              <select className="form__field js-convertFrom" required>
+                <option value="pln">PLN</option>
+                <option value="usd">USD</option>
+                <option value="gbp">GBP</option>
+                <option value="eur">EUR</option>
+                <option value="chf">CHF</option>
+              </select>
+            </label>
+          </p>
+          <p>
+            <label>
+              <span className="form__labelText">To:</span>
+              <select className="form__field js-convertTo" required>
+                <option value="pln">PLN</option>
+                <option value="usd" selected>
+                  USD
+                </option>
+                <option value="gbp">GBP</option>
+                <option value="eur">EUR</option>
+                <option value="chf">CHF</option>
+              </select>
+            </label>
+          </p>
+          <div className="form__buttons">
+            <p>
+              <button className="form__button js-form__button">Convert</button>
+            </p>
+            <p>
+              <button className="form__button" type="reset">
+                Reset
+              </button>
+            </p>
+          </div>
+          <p className="form__resultParagraph">
+            <label>
+              <span className="form__labelText">Result:</span>
+              <input
+                className="form__field form__field--bigger js-convertedValue"
+                type="number"
+                readonly
+              />
+            </label>
+          </p>
+        </form>
+      </main>
+      <footer className="footer">
+        <span>&copy Copyright Konrad Kasperczyk 2020 All Rights Reserved</span>
+      </footer>
+    </Container>
   );
 }
 
