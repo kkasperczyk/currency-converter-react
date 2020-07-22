c7def3cef0e13981aed3a23b024d283dd17beb18
Konrad Kasperczyk <kkasperczyk20@gmail.com>
2020-07-22 09:57:51 +0200

    Updates

---
+++ precache-manifest.ac77a774e6b648a118c6231e0dd12433.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06a370556cbc7a80e5bebb9d9943e418",
+    "url": "/currency-converter-react/index.html"
+  },
+  {
+    "revision": "16edd941b9f9d630f342",
+    "url": "/currency-converter-react/static/css/main.fa81ecc1.chunk.css"
+  },
+  {
+    "revision": "cb70966e9e6cb160b785",
+    "url": "/currency-converter-react/static/js/2.59662f4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency-converter-react/static/js/2.59662f4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16edd941b9f9d630f342",
+    "url": "/currency-converter-react/static/js/main.83d135ce.chunk.js"
+  },
+  {
+    "revision": "07b2973be230e9c120d7",
+    "url": "/currency-converter-react/static/js/runtime-main.2415008a.js"
+  }
+]);
