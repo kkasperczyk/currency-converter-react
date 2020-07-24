3d9863dcc2f1411fbf9011c4f7ef1975b4f6f250
Konrad Kasperczyk <kkasperczyk20@gmail.com>
2020-07-24 17:23:09 +0200

    Updates

--- precache-manifest.d769de6f6d0a5d988e114947fa88ebc2.js
+++ precache-manifest.d769de6f6d0a5d988e114947fa88ebc2.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "08e26c3d93278f7a299b12ca07847bcc",
+    "revision": "c4394c75daade654fd2c5491f0813b7a",
     "url": "/currency-converter-react/index.html"
   },
   {
-    "revision": "9fdf753ee58f8f8260a1",
+    "revision": "9cd7d24824c394f7e438",
     "url": "/currency-converter-react/static/css/main.fa81ecc1.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/currency-converter-react/static/js/2.59662f4b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9fdf753ee58f8f8260a1",
-    "url": "/currency-converter-react/static/js/main.4b8f2687.chunk.js"
+    "revision": "9cd7d24824c394f7e438",
+    "url": "/currency-converter-react/static/js/main.17a695bc.chunk.js"
   },
   {
     "revision": "07b2973be230e9c120d7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/currency-converter-react/precache-manifest.422bc1c67e5bfadb80b519dbc13a8528.js"
+  "/currency-converter-react/precache-manifest.d769de6f6d0a5d988e114947fa88ebc2.js"
 );
 
 self.addEventListener('message', (event) => {
