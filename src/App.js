2b7fc7ed0c045f3897d663ddbfa846a0318f5e26
Konrad Kasperczyk <kkasperczyk20@gmail.com>
2020-08-17 19:32:19 +0200

    calculateResult function simplified

--- src/App.js
+++ src/App.js
@@ -11,55 +11,16 @@ import { GlobalStyles } from "./GlobalStyles";
 function App() {
   const [result, setResult] = useState("");
 
+  const plnRates = {
+    PLN: 1,
+    EUR: 4.39,
+    USD: 3.71,
+    GBP: 4.85,
+    CHF: 4.08,
+  };
+
   const calculateResult = (convertFrom, convertTo, amount) => {
-    let plnValue;
-    let result;
-
-    const rates = {
-      eurRate: 4.4278,
-      usdRate: 3.9058,
-      gbpRate: 4.9384,
-      chfRate: 4.1187,
-    };
-
-    const { eurRate, usdRate, gbpRate, chfRate } = rates;
-    // eslint-disable-next-line default-case
-    switch (convertFrom) {
-      case "PLN":
-        plnValue = +amount;
-        break;
-      case "EUR":
-        plnValue = amount * eurRate;
-        break;
-      case "USD":
-        plnValue = amount * usdRate;
-        break;
-      case "GBP":
-        plnValue = amount * gbpRate;
-        break;
-      case "CHF":
-        plnValue = amount * chfRate;
-        break;
-    }
-
-    // eslint-disable-next-line default-case
-    switch (convertTo) {
-      case "PLN":
-        result = plnValue;
-        break;
-      case "EUR":
-        result = plnValue / eurRate;
-        break;
-      case "USD":
-        result = plnValue / usdRate;
-        break;
-      case "GBP":
-        result = plnValue / gbpRate;
-        break;
-      case "CHF":
-        result = plnValue / chfRate;
-        break;
-    }
+    const result = (amount * plnRates[convertFrom]) / plnRates[convertTo];
 
     setResult(`${result.toFixed(2)}`);
   };
